f062864c675010f521f15909f739b66f73b0aafb
Christian Chime <legacchris@gmail.com>
2019-01-03 23:09:28 +0100

    Add project files.

---
+++ ATM/Models/CheckingAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATM.Models
+{
+    public class CheckingAccount
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name="Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(10)]
+        [RegularExpression(@"\d{6,10}", ErrorMessage ="Account Number must be between 6 to 10 digits")]
+        [Display(Name ="Account #")]
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+
+        public string Name
+        {
+            get
+            {
+                return String.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+            
+        }
+
+    }
+}
